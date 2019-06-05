654e42a57379c0dc1cb51ebd91b3fe0ef134848f
halfrice <halfrice@pm.me>
2019-06-04 23:33:45 -0700

    Helmet

---
+++ src/components/seo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Helmet from "react-helmet"
+import favicon from "../images/icons/favicon.ico"
+
+const SEO = () => (
+  <Helmet>
+    <html lang="en" prefix="og: http://ogp.me/ns#" />
+    <title>Developer Website</title>
+    <link rel="shortcut icon" href={favicon} />
+  </Helmet>
+)
+
+export default SEO
